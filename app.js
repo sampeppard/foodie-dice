6e949fc78e2d2996a03fd3b2871f88b9d3c57eb2
sp <sampeppard@gmail.com>
2016-11-09 16:58:33 -0500

    rewrite the crud methods to talk to the mongo db

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-var listData = [
-  {
-    "id": 1,
-    "listName": "Spice Night!",
-    "ingredients": "['peppers', 'onions', 'cheese', 'cilantro', 'pork', 'beef']"
-  },
-  {
-    "id": 2,
-    "listName": "Sweet Night!",
-    "ingredients": "['vanilla extract', 'syrup', 'ice cream', 'chocolate', 'cookies', 'apples']"
-  }
-];
+// var listData = [
+//   {
+//     "id": 1,
+//     "listName": "Spice Night!",
+//     "ingredients": "['peppers', 'onions', 'cheese', 'cilantro', 'pork', 'beef']"
+//   },
+//   {
+//     "id": 2,
+//     "listName": "Sweet Night!",
+//     "ingredients": "['vanilla extract', 'syrup', 'ice cream', 'chocolate', 'cookies', 'apples']"
+//   }
+// ];
 
 // Load modules
 var bodyParser = require('body-parser');
@@ -27,9 +27,9 @@ var MongoClient = require("mongodb");
 // Connect to MongoDB
 var mongo = null;
 var lists = null;
-var mongoURL = "mongodb://" + config.mongoServer +
-  ":" + config.mongoPort + "/foodiedice";
-debug("Attempting connection to mongo @",mongoURL);
+var mongoURL = config.mongoURL;
+debug("Attempting connection to mongo @", mongoURL);
+//NOTE: Be careful with async here! Concurrency problems can occur
 MongoClient.connect(mongoURL, function(err, db) {
   if (err) {
     debug("ERROR:", err);
@@ -61,6 +61,7 @@ app.get('/', function (req, res) {
   res.send("Hello World!");
 });
 
+//NOTE: Watch out for concurrency issues between this server and mongodb
 // Start the server
 var port = process.env.PORT || 3000;
 debug("We picked up", port, "for the port");
@@ -70,98 +71,151 @@ var server = app.listen(port, "127.0.0.1", function () {
     console.log("Example app listening at http://%s:%s", host, port);
 });
 
-// Set up endpoint to grab all people
-var getAllLists = function(req, res) {
-  var response = listData;
-  // res.send(JSON.stringify(response));
-  res.status(200).jsonp(response);
-};
-app.get('/lists', getAllLists);
+/*------------MIDDLE WARE--------------*/
 
-
-//NOTE: ORDER OF DEFINITION MATTERS!! If callback is defined after endpoint then
-// route definition does not know that it is a callback
-// Endpoint for get individual lists
-var getLists = function(req, res) {
-  if (req.list) {
-    // res.send(200, JSON.stringify(req.list));
-    res.status(200).jsonp(req.list);
-  }
-  else {
-    res.send(400, { message: "Unrecognized identifier: " + identifier });
-  }
-}
-app.get('/lists/:listId', getLists);
+// MIDDLEWARE FOR ROUTES WITH DYNAMIC listID
 //NOTE: we can also get params object from the request object in the callback
 // but calling the param method allows us to do some filtering
 app.param('listId', function(req, res, next, listId) {
   debug("listId found:", listId);
-  var list = _.find(listData, function(it) { // this is _.find function is from lodash it returns the list objects that satisfy listId == it.id
-    return listId == it.id;
-  });
-  debug("list:", list);
-  req.list = list;
-  next();
+  if (mongodb.ObjectId.isValid(listId))
+  {
+    // Convert string version of listId into mongo type ObjectId
+    lists.find({"_id": new mongodb.ObjectId(listId) })
+      .toArray(function(err, docs) {
+        if (err)
+        {
+          debug("ERROR: listId:", err);
+          res.status(500).jsonp(err);
+        }
+        else if (docs.length < 1)
+        {
+          res.status(404).jsonp({ message: 'ID ' + listId + ' not found' });
+        }
+        else
+        {
+          debug("list:", docs[0]);
+          req.list = docs[0];
+          next();
+        }
+      });
+  }
+  else {
+    res.status(404).jsonp({ message: 'ID ' + listId + ' not found'});
+  }
 });
 
+/*------------MIDDLE WARE--------------*/
+
+// MIDDLEWARE DEPENDENT FUNCTIONS
+
+//NOTE: ORDER OF DEFINITION MATTERS!! If callback is defined after endpoint then
+// route definition does not know that it is a callback
+// Endpoint for get individual lists
+var getList = function(req, res) {
+  res.status(200).jsonp(req.person);
+};
+app.get('/lists/:listId', getList);
+
+// update list
+var updateList = function(req, res) {
+  debug("Updating", req.list, "with", req.body);
+  _.merge(req.list, req.body);
+  lists.updateOne({"_id":req.list._id}, req.list, function(err, result) {
+    if (err)
+    {
+      res.status(500).jsonp(err);
+    }
+    else
+    {
+      res.status(200).jsonp(result);
+    }
+  });
+};
+
+app.put('/lists/listId', updateList);
+
+// delete route
+var deleteList = function(req, res) {
+  debug("Removing", req.list.listName, req.list.ingredients);
+  lists.deleteOne({"_id": req.list._id}, function(err, result)
+  {
+    if (err)
+    {
+      debug("deleteList: ERROR:", err);
+      res.status(500).jsonp(err);
+    }
+    else
+    {
+      res.list._id = undefined;
+      res.status(200).jsonp(req.list);
+    }
+  });
+};
+app.delete('/lists/listId', deleteList);
+
+// MIDDLEWARE DEPENDENT FUNCTIONS
+
+// Set up endpoint to grab all lists
+var getAllLists = function(req, res) {
+  //NOTE: argument in find is a predicate document used to filter data
+  // e.g. {'listName': 'Spice Night!' } would return all data collections
+  // which satisfy the query criteria (aka with listName = Spice Night!)
+  lists.find({}).toArray(function(err, results) {
+    if (err)
+    {
+      debug("getAllLists--ERROR:", err);
+      res.status(500).jsonp(err);
+    }
+    else
+    {
+      debug("getAllLists:", results);
+      res.status(200).jsonp(results);
+    }
+  });
+};
+app.get('/lists', getAllLists);
 
 // add list
 var insertList = function(req, res) {
   var list = req.body;
   debug("Received", list);
-  list.id = listData.length + 1;
-  listData.push(list);
-  res.status(200).jsonp(list);
+  // MongoDB will create identifier field _id primary key
+
+  //NOTE: insertList function will terminate BEFORE database insert completes!
+  // DO NOT CALL res OUTSIDE OF CALLBACK TO MAKE SURE DB INSERT HAPPENS FIRST
+  lists.insert(list, function(err, result) {
+    if (err)
+    {
+      res.status(500).jsonp(err);
+    }
+    else
+    {
+      res.status(200).jsonp(list);
+    }
+    debug("INSIDE CALLBACK", list);
+  });
+
+  debug("OUTSIDE CALLBACK", list);
 };
 app.post('/lists', insertList);
 
-// update list
-var updateList = function(req, res) {
-  if (req.list) {
-    var originallist = req.list;
-    var incominglist = req.body;
-    var newlist = _.merge(originallist, incominglist);
-    res.status(200).jsonp(newlist);
-  } else {
-    res.status(404).jsonp({ message: "Unrecognized list identifier" });
-  }
-}
-app.put('/lists/listId', updateList);
-
-// delete route
-var deleteList = function(req, res) {
-  if (req.list) {
-    debug("Removing", req.list.listName, req.list.ingredients);
-    _.remove(listData, function(it) {
-      it.id === req.list.id;
-    });
-    debug("listData=", listData);
-    var response = { message: "Deleted successfully" };
-    res.status(200).jsonp(response);
-  } else {
-    var response = { message: "Unknown list identifier" };
-    res.status(404).jsonp(response);
-  }
-}
-app.delete('/lists/listId', deleteList);
-
-
-// delete all
-var deleteAllList = function(req, res) {
-  var list = listData;
-
-  if (Object.keys(list).length)
-  {
-    debug("Removing All Ingredients", list);
-    _.remove(list, function(it) {
-      return true;
-    });
-    var response = list;
-    res.status(200).jsonp(response);
-  }
-  else {
-    console.log("error");
-  }
-  // res.status(200).jsonp({ message: "test" });
-}
-app.delete('/lists', deleteAllList);
+// // delete all
+// var deleteAllList = function(req, res) {
+//   var list = listData;
+//
+//   if (Object.keys(list).length)
+//   {
+//     debug("Removing All Ingredients", list);
+//     _.remove(list, function(it) {
+//       return true;
+//     });
+//     var response = list;
+//     res.status(200).jsonp(response);
+//   }
+//   else {
+//     console.log("error");
+//   }
+//   // res.status(200).jsonp({ message: "test" });
+// }
+// app.delete('/lists', deleteAllList);
--- config.js
+++ config.js
@@ -9,6 +9,7 @@ for (var varIndex = 0; varIndex < envVarsKeys.length; varIndex++) {
   process.env[envVarsKeys[varIndex]] = envVars[envVarsKeys[varIndex]];
 }
 
+// Mongo config
 var debug = require('debug')('config');
 debug("Configuring environment...");
 // Use these as the default
@@ -16,8 +17,7 @@ module.exports = {
   mongoServer : "localhost",
   mongoPort : "27017"
 };
+
 if (process.env["ENV"] === "prod") {
-    module.exports.mongoServer = "sp:murica123@ds043942.mlab.com";
-  // module.exports.mongoServer = process.env["dbURI"];
-    module.exports.mongoPort = "43942";
+    module.exports.mongoURL = "mongodb://" + process.env["userName"] + ":" + process.env["password"] + "@" + process.env["mongoURI"] + ":" + process.env["dbPort"] + "/" + process.env["dbName"];
 }
